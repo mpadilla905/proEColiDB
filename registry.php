c495b72111fe96a2fb7b4f6d7f650375e5a9c23c
Monica Padilla <monicapadilla905@gmail.com>
2019-11-20 08:18:46 -0600

    fixed bugs

--- registry.php
+++ registry.php
@@ -19,7 +19,7 @@
 
         if( num[num.length-1] > 3){
           var campo = document.getElementById("mail").value;
-          if( !campo.match(/^\w{2,}@\w{2,}(\.\w{2,}){1,2}$/) ){
+          if( !campo.match(/^[a-zA-Z.]{2,}@\w{2,}(\.\w{2,}){1,2}$/) ){
             document.getElementById('mnr').style.visibility = "visible";
             flag = 0;
           }
@@ -44,8 +44,8 @@
       function clean(spanID) {
           document.getElementById(spanID).style.visibility = "hidden";
       }
-	//for checking email
-	        $(document).ready(function() {
+        //for checking email
+                $(document).ready(function() {
                 $("#resultadoBusqueda");
         });
         function buscar() {
@@ -58,8 +58,8 @@
                                 document.getElementById("message").style.visibility = "visible";
                       }
                         else {
-			       document.getElementById("registry_btn").disabled = false;
-				document.getElementById("message").style.visibility = "hidden";
+                               document.getElementById("registry_btn").disabled = false;
+                                document.getElementById("message").style.visibility = "hidden";
 
                       };
                  });
@@ -80,21 +80,16 @@
 
       <br><label for="user">User:</label>
        <input class="field" id="user" name="user" type="text" required="required" size="25" /></br>
-      <br><label for="mail">Mail:</label>
-       <input class="field" id="mail" name="mail" onchange="validate(4); buscar();" onclick="clean('mnr');" type="text" required="required" size="25" />
-       <span id ="mnr" style="visibility:hidden; color:#FF0000;" > Mail Not Recognized </span></br>
-      <br><label for="0">Name:</label>
-       <input class="field" id="0" name="name" onchange="validate(0)" onclick="clean('pelo0');" type="text" required="required" size="25" />
-       <span id ="pelo0" style="visibility:hidden; color:#FF0000;" > Please enter letters only </span></br>
-      <br><label for="1">Lastname:</label>
-       <input class="field" id="1" name="lastname" onchange="validate(1)" onclick="clean('pelo1');" type="text" required="required" size="25" />
-       <span id ="pelo1" style="visibility:hidden; color:#FF0000;" > Please enter letters only </span></br>
-      <br><label for="2">Institution of procedence:</label>
-       <input class="field" id="2" name="institute" onchange="validate(2)" onclick="clean('pelo2');" type="text" required="required" size="25" />
-       <span id ="pelo2" style="visibility:hidden; color:#FF0000;" > Please enter letters only </span></br>
-      <br><label for="3">Reason for dumping proEColiDB:</label>
+      <br><label for="mail">Mail:</label> <span id ="mnr" style="visibility:hidden; color:#FF0000;" > Mail Not Recognized </span></br>
+       <input class="field" id="mail" name="mail" onchange="validate(4); buscar();" onclick="clean('mnr');" type="text" required="required" size="25" /><br>
+      <br><label for="0">Name:</label> <span id ="pelo0" style="visibility:hidden; color:#FF0000;" > Please enter letters only </span></br>
+       <input class="field" id="0" name="name" onchange="validate(0)" onclick="clean('pelo0');" type="text" required="required" size="25" /><br>
+      <br><label for="1">Lastname:</label> <span id ="pelo1" style="visibility:hidden; color:#FF0000;" > Please enter letters only </span></br>
+       <input class="field" id="1" name="lastname" onchange="validate(1)" onclick="clean('pelo1');" type="text" required="required" size="25" /><br>
+      <br><label for="2">Institution of procedence:</label> <span id ="pelo2" style="visibility:hidden; color:#FF0000;" > Please enter letters only </span></br>
+       <input class="field" id="2" name="institute" onchange="validate(2)" onclick="clean('pelo2');" type="text" required="required" size="25" /><br>
+      <br><label for="3">Reason for dumping proEColiDB:</label><span id ="pelo3" style="visibility:hidden; color:#FF0000;" > Please enter letters only </span></br>
        <input class="field" id="3" name="reason" onchange="validate(3)" onclick="clean('pelo3');" type="text" required="required" size="25" />
-       <span id ="pelo3" style="visibility:hidden; color:#FF0000;" > Please enter letters only </span></br>
       <br></br>
       <div id="resultadoBusqueda"></div>
       <br><span id ="message" style="visibility:hidden; color:#FF0000;" > Mail Already Registered </span></br>
